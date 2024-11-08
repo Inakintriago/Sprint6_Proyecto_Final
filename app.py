6cfd4e2a830e7d67e695526a506ec161dad0541b
Inakintriago <inakintriago@gmail.com>
2024-11-08 16:16:23 -0600

    Actualización de la ubicación de app.py

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
+# Estabelcimiento de encabezados
+st.title('Bienvenido a mi Aplicación de Análisis de Vehículos')
+st.header('Histograma de Odómetro de Vehículos en USA')
+st.subheader('A continuación por favor selecciona lo que deseas visualizar')
+
 #Leer los datos
 car_data = pd.read_csv('vehicles_us.csv')
 
